a3900e803747f564cccb48d7fa52817aa676dc3a
Fabiano <fabianoquirinos@outlook.com>
2026-04-15 13:39:16 -0300

    :sparkless: feat: Implementing the final methods of Controller

--- account-service/src/main/java/com/example/finpay/account_service/controllers/AccountController.java
+++ account-service/src/main/java/com/example/finpay/account_service/controllers/AccountController.java
@@ -3,13 +3,12 @@ package com.example.finpay.account_service.controllers;
 
 import com.example.finpay.account_service.dto.account.AccountRequest;
 import com.example.finpay.account_service.dto.account.AccountResponse;
-import com.example.finpay.account_service.enums.AccountStatus;
 import com.example.finpay.account_service.services.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,7 +24,28 @@ public class AccountController {
         return ResponseEntity.ok().body(response);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<AccountResponse> findById(@PathVariable String id) {
+        AccountResponse response = accountService.findById(id);
+        return ResponseEntity.ok(response);
+    }
 
-    
+    @GetMapping(value = "/user/{userId}")
+    public ResponseEntity<List<AccountResponse>> findAllByUserId(@PathVariable String userId) {
+        List<AccountResponse> response = accountService.findAllByUserId(userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping(value = "/{id}/block")
+    public ResponseEntity<Void> blockAccount(@PathVariable String id) {
+        accountService.blockAccount(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping(value = "/{id}/activate")
+    public ResponseEntity<Void> activateAccount(@PathVariable String id) {
+        accountService.activateAccount(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
