752bc98ff53076b82e5c130164cc6735745765e9
Fabiano <fabianoquirinos@outlook.com>
2026-04-28 13:30:38 -0300

    :sparkless: feat: Adding the exception InsufficientBalance

--- payment-service/src/main/java/com/example/finpay/payment_service/services/TransactionService.java
+++ payment-service/src/main/java/com/example/finpay/payment_service/services/TransactionService.java
@@ -8,11 +8,13 @@ import com.example.finpay.payment_service.client.dto.payment.PaymentResponse;
 import com.example.finpay.payment_service.entities.Transaction;
 import com.example.finpay.payment_service.repositories.TransactionRepository;
 import com.example.finpay.payment_service.services.exceptions.AccountBlockedException;
+import com.example.finpay.payment_service.services.exceptions.InsufficientBalanceException;
 import com.example.finpay.payment_service.services.exceptions.SameAccountTransferException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -38,7 +40,11 @@ public class TransactionService {
         throw new AccountBlockedException(" The Account is not Active! ");
     }
 
+    if(request.value().compareTo(accountOrigin.balance()) > 0 || accountOrigin.balance().compareTo(BigDecimal.ZERO) <= 0){
+        throw new InsufficientBalanceException("The account : " + accountOrigin.id() + " Not have Balance , or the Value is more bigger than balance !" );
+    }
 
+    
 
 
         return null;
